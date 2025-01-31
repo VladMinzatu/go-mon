dfe7683402597f5cf20c3954c5dde9a9c0155cb1
Vlad Minzatu <minzatu.vlad@gmail.com>
2025-01-31 20:12:27 +0100

    template: Add static content on the page

--- web/handlers/home.go
+++ web/handlers/home.go
@@ -3,13 +3,29 @@ package handlers
 import (
 	"html/template"
 	"net/http"
+
+	"github.com/VladMinzatu/go-mon/monitor"
 )
 
-var tmpl = template.Must(template.ParseFiles("index.html"))
+var funcMap = template.FuncMap{
+	"toGB": func(bytes uint64) float64 {
+		return float64(bytes) / (1024 * 1024 * 1024)
+	},
+}
+
+var tmpl = template.Must(template.New("index.html").Funcs(template.FuncMap(funcMap)).ParseFiles("index.html"))
 
 func ServeHomepage(w http.ResponseWriter, r *http.Request) {
-	type data struct {
-		Heading string
+	stats := monitor.SystemStats{
+		CPUUsagePerCore: []float64{0, 0, 0, 0},
+		TotalMemory:     24000000000,
+		UsedMemory:      12000000000,
+		FreeMemory:      12000000000,
+		MemoryUsage:     0.5,
 	}
-	tmpl.Execute(w, data{Heading: "Heading is templated"})
+	err := tmpl.Funcs(funcMap).Execute(w, stats)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
