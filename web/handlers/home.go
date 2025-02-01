4a2eac977cb9164bd17179458157de08a8b367c7
Vlad Minzatu <minzatu.vlad@gmail.com>
2025-02-01 15:55:27 +0100

    display/update data in UI via websocket connection using htmx

--- web/handlers/home.go
+++ web/handlers/home.go
@@ -3,27 +3,12 @@ package handlers
 import (
 	"html/template"
 	"net/http"
-
-	"github.com/VladMinzatu/go-mon/monitor"
 )
 
-var funcMap = template.FuncMap{
-	"toGB": func(bytes uint64) float64 {
-		return float64(bytes) / (1024 * 1024 * 1024)
-	},
-}
-
-var tmpl = template.Must(template.New("index.html").Funcs(template.FuncMap(funcMap)).ParseFiles("web/views/index.html"))
+var homeTmpl = template.Must(template.New("index.html").ParseFiles("web/views/index.html"))
 
 func ServeHomepage(w http.ResponseWriter, r *http.Request) {
-	stats := monitor.SystemStats{
-		CPUUsagePerCore: []float64{0, 0, 0, 0},
-		TotalMemory:     24000000000,
-		UsedMemory:      12000000000,
-		FreeMemory:      12000000000,
-		MemoryUsage:     0.5,
-	}
-	err := tmpl.Funcs(funcMap).Execute(w, stats)
+	err := homeTmpl.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
